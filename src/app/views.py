feab33f410413092ada3dcbc297a65546b09b22f
tetsis <hitoshi.zin.nakao@gmail.com>
2018-11-03 23:48:46 +0900

    [add] 表とボタンを追加

--- src/app/views.py
+++ src/app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from model.models import File
  
 def index(request):
-    return render(request, 'index.html')
+    files = File.objects.all()
+
+    context = {
+        'files': files,
+    }
+    return render(request, 'index.html', context)
 
 def upload(request):
     return render(request, 'upload.html')
